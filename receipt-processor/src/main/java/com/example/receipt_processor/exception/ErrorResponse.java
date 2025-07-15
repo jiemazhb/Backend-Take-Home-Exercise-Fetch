51dcde12418a3301d6e2d22c89df3e29781ccc07
Danny Zhao <Danny.H.Zhao@gmail.com>
2025-07-14 23:15:42 -0700

    construct directory

--- receipt-processor/src/main/java/com/example/receipt_processor/exception/ErrorResponse.java
+++
@@ -1,4 +0,0 @@
-package com.example.receipt_processor.exception;
-
-public record ErrorResponse(String error) {
-}
--- receipt-processor/src/main/java/com/example/receipt_processor/model/ReceiptResponse.java
+++
@@ -1,4 +0,0 @@
-package com.example.receipt_processor.model;
-
-public record ReceiptResponse(String id) {
-}
